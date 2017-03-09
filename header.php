fc93c85293bc633f62dc6457fa7efe641ae3f8d8
karaBASSbaraBASS <evgen.mikhalov@gmail.com>
2017-03-10 01:52:25 +0200

    nav-bars

--- header.php
+++ header.php
@@ -24,13 +24,27 @@
 		<div id="nav">
 			<div class="container clearfix">
 				<div class="navigation left">
-					<ul>
+					<?php wp_nav_menu(array('theme_location'=>'menu', 'container'=>'false')); ?>
+
+				
+					<!-- <?php
+					$massiv_vhodnih_parametrov = array(
+						'container' => false,   // - без предварительной обертки тегом
+						'echo' => false,        // - вернуть меню для предварительной оработки средствами PHP
+						'items_wrap' => '%3$s', // - аргумент функции формата строки sprintf()
+						'depth' => 0,           // - глубина, уровень вложенности = 0
+					);
+					
+					/* удалим из строки меню все кроме html-тега и гиперссылки */
+					
+					print strip_tags( wp_nav_menu($massiv_vhodnih_parametrov ), '<a>') -->
+					<!-- <ul>
 						<li class="active"><span><a href="#">Portfolio</a></span></li>
 						<li><span><a href="#">Services</a></span></li>
 						<li><span><a href="#">Staff</a></span></li>
 						<li><span><a href="#">Articles</a></span></li>
 						<li><span><a href="#">Contacts</a></span></li>
-					</ul>
+					</ul> -->
 				</div>
 			</div>
 		</div>
